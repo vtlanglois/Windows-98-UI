4690aa9509f4e2d14bc12143b0d200139c620b56
Vincent Langlois <vtlanglo@gmail.com>
2024-01-19 22:29:41 -0500

    Added more to basic window demo

--- src/app/demos/basic-window/page.jsx
+++ src/app/demos/basic-window/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <div className="window container mx-auto mt-10">
         <div className="title-bar">
-          <div className="title-bar-text">A Window With Stuff In It, but new page!</div>
+          <div className="title-bar-text">Window Demo</div>
           <div className="title-bar-controls">
             <button aria-label="Minimize"></button>
             <button aria-label="Maximize"></button>
@@ -11,7 +11,13 @@ export default function Home() {
           </div>
         </div>
         <div className="window-body">
-          <p>There's so much room for activities!</p>
+          <p className="underline mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id minima
+            repudiandae assumenda distinctio, facere reiciendis provident
+            corrupti, quos tempore fugit saepe quam est. Quos earum dolore,
+            corrupti molestiae pariatur dolorem.
+          </p>
+          <button>Agree</button>
         </div>
       </div>
     </main>
